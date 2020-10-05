9e212ad6769a63e3610b4a74aeab3c867eca44c1
iandemed <iandemed@gmail.com>
2020-10-05 10:48:39 -0400

    Added djangorestframework package

--- final_project_django/settings.py
+++ final_project_django/settings.py
@@ -29,7 +29,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'final_project'
+    'final_project',
+    'rest_framework'
 ]
 
 MIDDLEWARE = [
