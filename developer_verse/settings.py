3d83c2ca11306ca4070071d27d47e54395663b1e
iandemed <iandemed@gmail.com>
2020-10-05 18:57:23 -0400

    Changed local database to PostgreSQL and added packages for heroku deployment

--- developer_verse/settings.py
+++ developer_verse/settings.py
@@ -75,8 +75,11 @@ WSGI_APPLICATION = 'developer_verse.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+       'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'final_project',
+        'USER': 'final_project_user',
+        'PASSWORD': 'final_project',
+        'HOST': 'localhost'
     }
 }
 
