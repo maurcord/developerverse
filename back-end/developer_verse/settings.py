9443ad1453b42c0583180710a3efb80099f41657
absurdlyeloquent <ethan.henderson397@gmail.com>
2020-10-06 16:58:06 -0400

    added env variables

--- back-end/developer_verse/settings.py
+++ back-end/developer_verse/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -20,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '(cqfb+6om^5)p*a^t33(-l)y2v1ul1f)!2img0++b_l%it%_$+'
+SECRET_KEY = os.getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -70,19 +71,22 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'developer_verse.wsgi.application'
 
+# env vari's
+from dotenv import load_dotenv
+load_dotenv()
 
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
+
 
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'developerverse',
         'USER': 'developerverse_user',
-        # At some point, these should be env vari's
-        'PASSWORD': 'developerverse',
-        'HOST': 'localhost',
-        'PORT': '5432'
+        'PASSWORD': os.getenv("DB_PWD"),
+        'HOST': os.getenv("DB_HOST"),
+        'PORT': os.getenv("DB_PORT")
     }
 }
 
